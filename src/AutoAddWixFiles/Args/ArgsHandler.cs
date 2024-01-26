69a4a0e5cb94e260c8c5bdf363cf902b14c173c3
soliddrogue <savyakb@gmail.com>
2024-01-26 16:26:02 -0700

    AddDLLToWixFiles

---
+++ src/AutoAddWixFiles/Args/ArgsHandler.cs
@@ -0,0 +1,56 @@
+﻿using Rhyous.SimpleArgs;
+
+namespace AutoAddWixFiles.Arguments
+{
+    // Add this line of code to Main() in Program.cs
+    //
+    //   new ArgsManager<ArgsHandler>().Start(args);
+    //
+
+    /// <summary>
+    /// A class that implements IArgumentsHandler where command line
+    /// arguments are defined.
+    /// </summary>
+    public sealed class ArgsHandler : ArgsHandlerBase
+    {
+        public override void InitializeArguments(IArgsManager argsManager)
+        {
+            Arguments.AddRange(new List<Argument>
+            {
+                new Argument
+                {
+                    Name = "FileList",
+                    ShortName = "FL",
+                    Description = "The list of wixproj files.",
+                    Example = "{name}=.\\list\\list.txt",
+                    IsRequired = true
+                },
+                new Argument
+                {
+                    Name = "PrototypeDLL",
+                    ShortName = "PD",
+                    Description = "DLL that are already in WXS files.",
+                    Example = "{name}=example.dll,example2.dll",
+                    IsRequired = true
+                },
+                 new Argument
+                {
+                    Name = "DLL",
+                    ShortName = "D",
+                    Description = "DLL to add.",
+                    Example = "{name}=example.dll",
+                    IsRequired = true
+                },
+                
+
+                new ConfigFileArgument(argsManager) // This is a special Argument type to allow for args in a file
+            });
+        }
+
+        public override void HandleArgs(IReadArgs inArgsHandler)
+        {
+            base.HandleArgs(inArgsHandler);
+            Console.WriteLine("I handled the args!!!");
+        }
+    }
+}
---
+++ src/AutoAddWixFiles/Program.cs
@@ -0,0 +1,98 @@
+﻿using AutoAddWixFiles.Arguments;
+using Rhyous.SimpleArgs;
+using System.Net.WebSockets;
+
+const string Template = "{0}<File Id=\"{1}\" Name=\"{1}\" Source=\"$(var.{2}.TargetDir){1}\" />";
+
+
+
+new ArgsManager<ArgsHandler>().Start(args);
+var fileList = Args.Value("FL");
+var dll = Args.Value("D");
+var prototypeDLLs = Args.Value("PD").Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries); 
+
+
+var files = File.ReadAllLines(fileList);
+var dirs = files.Select(f=>Path.GetDirectoryName(f)).ToList();
+foreach (var dir in dirs)
+{
+    if (!Directory.Exists(dir))
+    {
+        Console.WriteLine($"This directory doesn't exist: {dir} ");
+        return;
+    }
+    var wxsFiles = Directory.GetFiles(dir, $"*.wxs", SearchOption.AllDirectories);
+
+    foreach (var wxsFile in wxsFiles)
+    {
+        var lines = File.ReadAllLines(wxsFile);
+        var newFileLines = new List<string>();
+        var i = 0;
+        var line = lines[i];
+        var doneWithWxsFile = false;
+        // Write all lines before pattern found
+        while (!line.Contains("Source=") || !prototypeDLLs.Any(dll => line.Contains($"){dll}")))
+        {
+            newFileLines.Add(line);
+            i++;
+            if (i < lines.Length)
+            {
+                line = lines[i];
+            }
+            else
+            {
+                doneWithWxsFile= true;
+                break;
+            }
+        }
+
+        if (doneWithWxsFile)
+        {
+            continue;
+        }
+
+        var projName = ParseForProjName(line);
+        var leadingWhiteSpace = ParseForWhiteSpace(line);
+
+        while (!line.Contains("/>"))
+        {
+            newFileLines.Add(line);
+            line = lines[++i];
+        }
+        newFileLines.Add(line);
+        line = lines[++i];
+        newFileLines.Add(string.Format(Template, leadingWhiteSpace , dll, projName));
+
+
+        //move to end of file
+        while (i < lines.Length)
+        {
+
+            newFileLines.Add(line);
+            i++;
+            if (i < lines.Length)
+                line = lines[i];
+        }
+        File.WriteAllLines(wxsFile, newFileLines);
+            break;
+    }
+
+}
+
+static string ParseForProjName(string line)
+{
+    var projNameStart = line.IndexOf("$(var.");
+    var projNameEnd = line.IndexOf(".TargerDir", projNameStart);
+    if (projNameStart != -1 && projNameEnd != -1)
+    {
+
+        return line.Substring(projNameStart + 6, projNameEnd - projNameStart - 6);
+    }
+    return "";
+}
+
+static string ParseForWhiteSpace(string line)
+{
+    return line.Substring(0, line.IndexOf('<'));
+
+}
