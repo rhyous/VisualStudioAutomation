4a11503eb425c127a47c070204efcd8118c1e487
Jared Barneck <Jared.Barneck@niceincontact.com>
2024-02-12 18:45:09 -0700

    Add project to compare dll in installers

---
+++ src/MsiFileInfoReader/Args/ArgsHandler.cs
@@ -0,0 +1,64 @@
+﻿using Rhyous.SimpleArgs;
+using System;
+using System.Collections.Generic;
+
+namespace Rhyous.MsiFileInfoReader.Arguments
+{
+    // Add this line of code to Main() in Program.cs
+    //
+    //   new ArgsManager<ArgsHandler>().Start(args);
+    //
+
+    /// <summary>
+    /// A class that implements IArgumentsHandler where command line
+    /// arguments are defined.
+    /// </summary>
+    public sealed class ArgsHandler : ArgsHandlerBase
+    {
+        public override void InitializeArguments(IArgsManager argsManager)
+        {
+            Arguments.AddRange(new List<Argument>
+            {
+                new Argument
+                {
+                    Name = "TokenizedFolder",
+                    ShortName = "TF",
+                    Description = "The folder path with a tokenized directory for MSI compare.",
+                    Example = "{name}=c:\\{0}",
+                    IsRequired = true
+                },
+                new Argument
+                {
+                    Name = "LeftFolder",
+                    ShortName = "LF",
+                    Description = "The left folder for MSI compare.",
+                    Example = "{name}=left",
+                    IsRequired = true
+                },
+                new Argument
+                {
+                    Name = "RightFolder",
+                    ShortName = "RF",
+                    Description = "The right folder for MSI compare.",
+                    Example = "{name}=right",
+                    IsRequired = true
+                },
+                new Argument
+                {
+                    Name = "File",
+                    ShortName = "F",
+                    Description = "The file to find in the MSI and compare",
+                    Example = "{name}=example.dll",
+                    IsRequired = true
+                },
+                new ConfigFileArgument(argsManager) // This is a special Argument type to allow for args in a file
+            });
+        }
+
+        public override void HandleArgs(IReadArgs inArgsHandler)
+        {
+            base.HandleArgs(inArgsHandler);
+            Console.WriteLine("I handled the args!!!");
+        }
+    }
+}
---
+++ src/MsiFileInfoReader/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using WixToolset.Dtf.WindowsInstaller;
+using Rhyous.SimpleArgs;
+using Rhyous.MsiFileInfoReader.Arguments;
+using System.IO;
+
+
+namespace Rhyous.MsiFileInfoReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ArgsManager<ArgsHandler>().Start(args);
+            var tokenizedFolder = Args.Value("TF");
+            var leftFolder = Args.Value("LF");
+            var rightFolder = Args.Value("RF");
+            var file = Args.Value("F");
+
+            var leftFolderFullPath = string.Format(tokenizedFolder, leftFolder);
+            var rightFolderFullPath = string.Format(tokenizedFolder, rightFolder);
+
+            var msiFiles = Directory.GetFiles(leftFolderFullPath, "*.msi", SearchOption.AllDirectories);
+            var msiFileFinder = new MSIFileFinder();
+            foreach (var msiFile in msiFiles)
+            {
+                var leftMsiFile = msiFileFinder.Find(msiFile, file);
+                if (leftMsiFile == null)
+                { continue; }
+
+                Console.WriteLine($"Side:  Left, Msi: {Path.GetFileName(msiFile)}, File: {file}, Version: {leftMsiFile.Version}, Directory: {leftMsiFile.Folder}");
+
+                var rightFile = msiFile.Replace(leftFolderFullPath, rightFolderFullPath);
+                var rightMsiFile = msiFileFinder.Find(rightFile, file);
+                if (rightMsiFile != null)
+                {
+                    Console.WriteLine($"Side: Right, Msi: {Path.GetFileName(msiFile)}, File: {file}, Version: {rightMsiFile.Version.PadLeft(12, ' ')}, Directory: {rightMsiFile.Folder}");
+                }
+            }
+
+        }
+    }
+
+    public class MsiFile
+    {
+        public Record FileRecord { get; set; }
+        public string Version { get; set; }
+        public Record ComponentRecord { get; set; }
+        public string Folder { get; set; }
+    }
+
+    public class MSIFileFinder
+    {
+        public MsiFile Find(string msiPath, string fileNameToFind)
+        {
+            using (var database = new Database(msiPath, DatabaseOpenMode.ReadOnly))
+            {
+                // Query the File table to find the row for the specified file
+                string fileQuery = $"SELECT `Component_`, `Version` FROM `File` WHERE `FileName` = '{fileNameToFind}' OR `FileName` = '{fileNameToFind}|'";
+                using (var fileView = database.OpenView(fileQuery))
+                {
+                    fileView.Execute();
+                    if (fileView.Fetch() is Record fileRecord)
+                    {
+                        string component = fileRecord["Component_"].ToString();
+
+                        // Query the Component table to get the directory associated with the component
+                        string componentQuery = $"SELECT `Directory_` FROM `Component` WHERE `Component` = '{component}'";
+                        using (var componentView = database.OpenView(componentQuery))
+                        {
+                            componentView.Execute();
+                            if (componentView.Fetch() is Record componentRecord)
+                            {
+                                return new MsiFile
+                                {
+                                    FileRecord = fileRecord,
+                                    Version = fileRecord["Version"].ToString(),
+                                    ComponentRecord = componentRecord,
+                                    Folder = componentRecord["Directory_"].ToString(),
+                                };
+
+                                // You may need to resolve the directory to a full path by traversing the Directory table
+                                // This example simply prints the directory ID
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
